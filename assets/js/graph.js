c93be4dc2d602f2847310c627b1a4144e69e933e
Serfuz <60694014+Serfuz@users.noreply.github.com>
2026-05-25 14:54:45 +0200

    Update graph.js

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -150,7 +150,7 @@ function initGraph(rawRecords) {
 
   setTimeout(() => {
     network.moveTo({
-      scale: 0.4, // ✅ zoom OUT initially
+      scale: 1, // ✅ zoom OUT initially
       position: { x: 0, y: 0 },
       animation: false
     });
