dbaef04b3dc49545f02143073616e2ef4e2c5fee
Serfuz <60694014+Serfuz@users.noreply.github.com>
2026-05-25 13:59:45 +0200

    Update graph.js

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -67,8 +67,8 @@ function initGraph(rawRecords) {
           url: url,
           size: levelSizes[i] || 10,
           color: levelColors[i] || "#ccc",
-          x: 0,
-          y: 0,
+          x: (Math.random() - 0.5) * 1,
+          y: (Math.random() - 0.5) * 1,
           font: {
             size: levelFontSizes[i] || 24
           }
